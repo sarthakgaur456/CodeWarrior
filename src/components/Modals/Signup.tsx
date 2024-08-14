26cdbeb16ed57538ec9fefd1c442b48e52e5e1ae
sarthakgaur456 <sarthakgaur456@gmail.com>
2024-08-14 17:30:38 +0530

    xxx

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,49 +1,59 @@
-import { authModalState } from '@/atoms/authModalAtom';
-import React, { useEffect, useState } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/firebase';
-import { useRouter } from 'next/router';
+import { authModalState } from "@/atoms/authModalAtom";
+import { auth, firestore } from "@/firebase/firebase";
+import { useEffect, useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
-type SignupProps = {
-    
-};
+type SignupProps = {};
 
-const Signup:React.FC<SignupProps> = () => {
-    const setAuthModalState = useSetRecoilState(authModalState);
+const Signup: React.FC<SignupProps> = () => {
+	const setAuthModalState = useSetRecoilState(authModalState);
 	const handleClick = () => {
 		setAuthModalState((prev) => ({ ...prev, type: "login" }));
 	};
 	const [inputs, setInputs] = useState({ email: "", displayName: "", password: "" });
 	const router = useRouter();
-	const [
-		createUserWithEmailAndPassword,
-		user,
-		loading,
-		error,
-	  ] = useCreateUserWithEmailAndPassword(auth);
-	  
+	const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
 	const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
 	};
+
 	const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		if (!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields");
-		try{
+		try {
+			toast.loading("Creating your account", { position: "top-center", toastId: "loadingToast" });
 			const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
 			if (!newUser) return;
+			const userData = {
+				uid: newUser.user.uid,
+				email: newUser.user.email,
+				displayName: inputs.displayName,
+				createdAt: Date.now(),
+				updatedAt: Date.now(),
+				likedProblems: [],
+				dislikedProblems: [],
+				solvedProblems: [],
+				starredProblems: [],
+			};
+			await setDoc(doc(firestore, "users", newUser.user.uid), userData);
 			router.push("/");
 		} catch (error: any) {
-			alert(error.message);
+			toast.error(error.message, { position: "top-center" });
+		} finally {
+			toast.dismiss("loadingToast");
 		}
 	};
+
 	useEffect(() => {
 		if (error) alert(error.message);
 	}, [error]);
-	
 
-    return (
-        <form className='space-y-6 px-6 pb-4' onSubmit={handleRegister} >
+	return (
+		<form className='space-y-6 px-6 pb-4' onSubmit={handleRegister}>
 			<h3 className='text-xl font-medium text-white'>Register to LeetClone</h3>
 			<div>
 				<label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
@@ -105,11 +115,11 @@ const Signup:React.FC<SignupProps> = () => {
 
 			<div className='text-sm font-medium text-gray-300'>
 				Already have an account?{" "}
-				<a href='#' className='text-blue-700 hover:underline' onClick={handleClick} >
+				<a href='#' className='text-blue-700 hover:underline' onClick={handleClick}>
 					Log In
 				</a>
 			</div>
 		</form>
-    );
-}
+	);
+};
 export default Signup;
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,24 +1,49 @@
-import { auth } from '@/firebase/firebase';
-import Link from 'next/link';
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Logout from '../Buttons/Logout';
-import { authModalState } from '@/atoms/authModalAtom';
-import { useSetRecoilState } from 'recoil';
-import Image from 'next/image';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import { BsList } from 'react-icons/bs';
+import { auth } from "@/firebase/firebase";
+import Link from "next/link";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
+import Image from "next/image";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsList } from "react-icons/bs";
+import Timer from "../Timer/Timer";
+import { useRouter } from "next/router";
+import { problems } from "@/utils/problems";
+import { Problem } from "@/utils/types/problem";
 
 type TopbarProps = {
-    problemPage?: boolean;
+	problemPage?: boolean;
 };
 
-const Topbar:React.FC<TopbarProps> = (problemPage) => {
-    const [user] = useAuthState(auth);
-    const setAuthModalState = useSetRecoilState(authModalState);
-    return (
-        <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
-			<div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""} `}>
+const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
+	const [user] = useAuthState(auth);
+	const setAuthModalState = useSetRecoilState(authModalState);
+	const router = useRouter();
+
+	const handleProblemChange = (isForward: boolean) => {
+		const { order } = problems[router.query.pid as string] as Problem;
+		const direction = isForward ? 1 : -1;
+		const nextProblemOrder = order + direction;
+		const nextProblemKey = Object.keys(problems).find((key) => problems[key].order === nextProblemOrder);
+
+		if (isForward && !nextProblemKey) {
+			const firstProblemKey = Object.keys(problems).find((key) => problems[key].order === 1);
+			router.push(`/problems/${firstProblemKey}`);
+		} else if (!isForward && !nextProblemKey) {
+			const lastProblemKey = Object.keys(problems).find(
+				(key) => problems[key].order === Object.keys(problems).length
+			);
+			router.push(`/problems/${lastProblemKey}`);
+		} else {
+			router.push(`/problems/${nextProblemKey}`);
+		}
+	};
+
+	return (
+		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
+			<div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""}`}>
 				<Link href='/' className='h-[22px] flex-1'>
 					<Image src='/logo-full.png' alt='Logo' height={100} width={100} />
 				</Link>
@@ -27,7 +52,7 @@ const Topbar:React.FC<TopbarProps> = (problemPage) => {
 					<div className='flex items-center gap-4 flex-1 justify-center'>
 						<div
 							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
-							// onClick={() => handleProblemChange(false)}
+							onClick={() => handleProblemChange(false)}
 						>
 							<FaChevronLeft />
 						</div>
@@ -42,7 +67,7 @@ const Topbar:React.FC<TopbarProps> = (problemPage) => {
 						</Link>
 						<div
 							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
-							// onClick={() => handleProblemChange(true)}
+							onClick={() => handleProblemChange(true)}
 						>
 							<FaChevronRight />
 						</div>
@@ -60,14 +85,6 @@ const Topbar:React.FC<TopbarProps> = (problemPage) => {
 							Premium
 						</a>
 					</div>
-                    {/* <Link
-							href='/auth'
-							// onClick={() => setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }))}
-						>
-							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
-						</Link> */}
-
-
 					{!user && (
 						<Link
 							href='/auth'
@@ -76,10 +93,10 @@ const Topbar:React.FC<TopbarProps> = (problemPage) => {
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 						</Link>
 					)}
-					{/* {user && problemPage && <Timer />} */}
+					{user && problemPage && <Timer />}
 					{user && (
 						<div className='cursor-pointer group relative'>
-							<img src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
+							<Image src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
 							<div
 								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
 								z-40 group-hover:scale-100 scale-0 
@@ -93,6 +110,6 @@ const Topbar:React.FC<TopbarProps> = (problemPage) => {
 				</div>
 			</div>
 		</nav>
-    );
-}
+	);
+};
 export default Topbar;
